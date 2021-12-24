5c7f753f278c6075e3fbf21be1f0a3b58e04400f
Andrei Kulbeda <andrei_kulbeda@epam.com>
2021-12-24 12:23:49 +0300

    FLUXUI-22946 Migrate Flutter from 2.5.3 to 2.8.1 version AppleTV. fix issues with #if #endif

--- src/gpu/graphite/mtl/MtlCaps.mm
+++ src/gpu/graphite/mtl/MtlCaps.mm
@@ -181,8 +181,6 @@ bool MtlCaps::GetGPUFamily(id<MTLDevice> device, GPUFamily* gpuFamily, int* grou
 #if SKGPU_GRAPHITE_METAL_SDK_VERSION >= 220
     if (@available(macOS 10.15, iOS 13.0, tvOS 13.0, *)) {
         // Apple Silicon
-#if !(defined(TARGET_OS_TV) && TARGET_OS_TV)
-#if SKGPU_GRAPHITE_METAL_SDK_VERSION >= 230
         if ([device supportsFamily:MTLGPUFamilyApple7]) {
             *gpuFamily = GPUFamily::kApple;
             *group = 7;
@@ -246,8 +244,12 @@ bool MtlCaps::GetGPUFamily(id<MTLDevice> device, GPUFamily* gpuFamily, int* grou
         }
 #endif
     }
+#endif
 #else
-        if ([device supportsFamily:MTLGPUFamilyApple5]) {
+
+#if GR_METAL_SDK_VERSION >= 220
+    if (@available(macOS 10.15, iOS 13.0, tvOS 13.0, *)) {
+         if ([device supportsFamily:MTLGPUFamilyApple5]) {
             *gpuFamily = GPUFamily::kApple;
             *group = 5;
             return true;
@@ -272,7 +274,7 @@ bool MtlCaps::GetGPUFamily(id<MTLDevice> device, GPUFamily* gpuFamily, int* grou
             *group = 1;
             return true;
         }
-
+    }
 #endif
 #endif
 
